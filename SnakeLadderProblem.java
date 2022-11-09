02c0ca7bdc2cf0b242c2089ccb5684f6077caf84
gaurav459433 <gaurav459433@gmail.com>
2022-11-09 21:40:27 +0530

    uc-1 Player Starting Position

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -3,5 +3,8 @@ package com.bridgelabz.snakeladder;
 public class SnakeLadderProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Sanke Ladder Problem");
+        System.out.println();
+        int Singleplayer=0;
+        System.out.println("Starting position of Single Player is: "+Singleplayer);
     }
 }
