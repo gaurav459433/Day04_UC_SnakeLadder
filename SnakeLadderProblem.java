e8c12b7b8c7528b73ff98aaf6f4052015df54718
gaurav459433 <gaurav459433@gmail.com>
2022-11-10 21:33:59 +0530

    uc4-Repeat till position 100

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -10,22 +10,29 @@ public class SnakeLadderProblem {
         int Singleplayer = 0;
         System.out.println("Starting position of Single Player is: " + Singleplayer);
 
-        int SingleplayerRolls = random.nextInt(6) + 1;
-        int a = random.nextInt(3);
+        while (Singleplayer != 100) {
+            int SingleplayerRolls = random.nextInt(6) + 1;
+            int a = random.nextInt(3);
+            System.out.println();
+            System.out.println("The Player rolls the die & get a number: " + SingleplayerRolls);
+            if (a == 0) {
+                System.out.println("No play - stays in the same position");
+            }
+            if (a == 1) {
+                Singleplayer += SingleplayerRolls;
+                System.out.println("Gets Ladder & Player moves ahead by number of position: " + SingleplayerRolls);
+            }
+            if (a == 2) {
+                Singleplayer -= SingleplayerRolls;
+                if (Singleplayer <= 0) {
+                    Singleplayer = 0;
+                    System.out.println("Snake bytes & Player moves Behind by number of position: " + "-" + SingleplayerRolls);
+                }
+                else
+                    System.out.println("Snake bytes & Player moves Behind by number of position: " + "-" + SingleplayerRolls);
+            }
+        }
         System.out.println();
-        System.out.println("The Player rolls the die & get a number: " + SingleplayerRolls);
-        if (a == 0) {
-            System.out.println("No play - stays in the same position");
-        }
-        if (a == 1) {
-            Singleplayer += SingleplayerRolls;
-            System.out.println("Gets Ladder & Player moves ahead by number of position: " + SingleplayerRolls);
-            System.out.println("Player current position: " + Singleplayer);
-        }
-        if (a == 2) {
-            Singleplayer -= SingleplayerRolls;
-            System.out.println("Snake bytes & Player moves Behind by number of position: " + "-" + SingleplayerRolls);
-            System.out.println("Player current position: " + Singleplayer);
-        }
+        System.out.println("Player Reaches position: " + Singleplayer);
     }
 }
